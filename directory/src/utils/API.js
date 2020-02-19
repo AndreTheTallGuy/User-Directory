52e5afbbcd5cb0bb933fb691779f733831a37b0f
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-19 16:38:10 -0600

    cleaning code

--- directory/src/utils/API.js
+++ directory/src/utils/API.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const BASEURL = "https://randomuser.me/api/?results=100&nat=us";
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   getPeople: function() {
     return axios.get(BASEURL);
