ef20093b9c0454f2f7ee02d388f49b44ca663a7d
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-19 17:16:10 -0600

    heroku

--- directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASEURL = "https://randomuser.me/api/?results=100&nat=us";
-
-export default {
-  getPeople: function() {
-    return axios.get(BASEURL);
-  }
-};
